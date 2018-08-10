a5ffab4045cab0e4787cc0921d46a53d476b88bb
Lee Vaughn <32204461+LeeVaughn@users.noreply.github.com>
2018-08-09 21:57:29 -0700

    Update scripts.js
    Refactored code to use const/let and template literals

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-var names = ["freecodecamp", "csharpfritz", "blizzheroes", "noopkat", "noobs2ninjas", "vtriple", "supremerumham", "sheriffjackson", "jesseskinner", "esl_sc2", "dunktrain"];
-var displayName, logo;
+const names = ["freecodecamp", "csharpfritz", "blizzheroes", "noopkat", "noobs2ninjas", "vtriple", "supremerumham", "sheriffjackson", "jesseskinner", "esl_sc2", "dunktrain"];
+let displayName, logo;
 
 names.forEach((name) => {
-    var channelAPI = "https://wind-bow.gomix.me/twitch-api/channels/" + name + "?callback=?";
-    var streamAPI = "https://wind-bow.gomix.me/twitch-api/streams/" + name + "?callback=?";
+    const channelAPI = `https://wind-bow.gomix.me/twitch-api/channels/${name}?callback=?`;
+    const streamAPI = `https://wind-bow.gomix.me/twitch-api/streams/${name}?callback=?`;
 
     $.getJSON(channelAPI, function (channel) {
         $.getJSON(streamAPI, function (stream) {
@@ -12,20 +12,32 @@ names.forEach((name) => {
             // that have special characters in their display_name
             displayName = channel.display_name;
             displayNameRegex = displayName.replace(/[^A-Z0-9_]/ig, "");
-            logo = "<img src='" + channel.logo + "' class='logo'>";
+            logo = `<img src="${channel.logo}" class="logo">`;
 
             if (stream.stream != null) {
-                $("#names").prepend("<div class='name on' id='" + displayNameRegex + "'>");
-                $("#" + displayNameRegex).append("<a href='//twitch.tv/" + name + "' target='_blank'>" + logo + "<h2 class='displayName'>" + displayName + "</h2></div>" + "<div class='game'>" + "Currrently streaming <strong>" + stream.stream.channel.game + "</strong> for " + stream.stream.viewers + " viewers.</div>" + "<div class='topic'>" + "<em>" + stream.stream.channel.status + "</em></div></a>");
+                $("#names").prepend(`
+                    <div class="name on" id="${displayNameRegex}">
+                        <a href="//twitch.tv/${name}" target="_blank">${logo}
+                            <h2 class="displayName">${displayName}</h2>
+                            <div class="game">Currrently streaming <strong>${stream.stream.channel.game}</strong> for ${stream.stream.viewers} viewers.</div>
+                            <div class="topic"><em>${stream.stream.channel.status}</em></div>
+                        </a>
+                    </div>
+                `);
             } else {
-                $("#names").append("<div class='name off' id='" + displayNameRegex + "'>");
-                $("#" + displayNameRegex).append("<a href='//twitch.tv/" + name + "' target='_blank'>" + logo + "<h2 class='displayName'>" + displayName + "</h2></div></a>");
+                $("#names").append(`
+                    <div class="name off" id="${displayNameRegex}">
+                        <a href="//twitch.tv/${name}" target="_blank">${logo}
+                        <h2 class="displayName">${displayName}</h2>
+                        </a>
+                    </div>
+                `);
             }
 
             if (channel.logo === null) {
                 channel.logo = "http://www-cdn.jtvnw.net/images/xarth/404_user_150x150.png";
             }
-
+    
         });
 
         $("#all").click(function () {
